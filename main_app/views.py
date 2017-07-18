6caa6e4fca6bc7786390e544867148733041bd1d
CCrowRun <carrie.g.crow@gmail.com>
2017-07-18 13:14:08 -0500

    Updated styling and content
    Try Django: level 2
    Looped over python list in HTML
    Added Bootstrap to static
    Updated data styling

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse('<h1>Hello Explorers!</h1>')
+	return render(request, 'index.html', {'treasures': treasures})
+
+class Treasure:
+	def __init__(self, name, value, material, location):
+		self.name = name
+		self.value = value
+		self.material = material
+		self.location = location
+
+treasures = [
+	Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM"),
+	Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO"),
+	Treasure('Coffee Can', 20.00, 'tin', 'Acme, CA'),
+]
