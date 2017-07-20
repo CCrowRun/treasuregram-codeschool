c5904e14f0217743bac5848abe00bf4361adfe7c
CCrowRun <carrie.g.crow@gmail.com>
2017-07-20 11:26:10 -0500

    Lesson 1.4 - Detail page
    Added linking to detail page for each treasure

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index),
+	url(r'^([0-9]+)/$',
+		views.detail, name = 'detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,7 @@ from .models import Treasure
 def index(request):
 	treasures = Treasure.objects.all()
 	return render(request, 'index.html', {'treasures': treasures})
+
+def detail(request, treasure_id):
+	treasure = Treasure.objects.get(id=treasure_id)
+	return render(request, 'detail.html', {'treasure': treasure})
