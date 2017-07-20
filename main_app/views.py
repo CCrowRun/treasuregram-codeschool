f8abcf1e1aef1f1e84586f369cc7799e984b0403
CCrowRun <carrie.g.crow@gmail.com>
2017-07-20 15:48:24 -0500

    Created Images
    Created images and a few new test objects

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .models import Treasure
 from .forms import TreasureForm
 
